bbb305886c316118a6ae2e4de16eb8fcec6330f2
joshparkerj <superfakek@gmail.com>
2018-08-02 02:04:01 -0600

    Add detailed tutorial (incomplete) and make various small tweaks

--- pyglet/si.py
+++ pyglet/si.py
@@ -2,31 +2,29 @@ import pyglet
 from random import random
 from random import randint
 
-gameTime = 0
 playerScore = 0
 gameOn = True
 
-window=pyglet.window.Window(500,500)
+window = pyglet.window.Window(500,500)
 
 batchDraw = pyglet.graphics.Batch()
 
 bgroup = pyglet.graphics.OrderedGroup(0)
 fgroup = pyglet.graphics.OrderedGroup(1)
 
-playerImage = pyglet.resource.image('player.png')
-spaceImage = pyglet.resource.image('space.png')
-bulletImage = pyglet.resource.image('bullet.png')
-badImages = []
-badImages.append(pyglet.resource.image('bad1.png'))
-badImages.append(pyglet.resource.image('bad2.png'))
-badImages.append(pyglet.resource.image('bad3.png'))
-badImages.append(pyglet.resource.image('bad4.png'))
-badImages.append(pyglet.resource.image('bad5.png'))
+img = pyglet.resource.image
 
-zapSound = pyglet.resource.media('zap.wav', streaming=False)
-blastSound = pyglet.resource.media('blast.wav', streaming=False)
+playerImage = img('player.png')
+spaceImage = img('space.png')
+bulletImage = img('bullet.png')
+badImages = [img('bad1.png'),img('bad2.png'),img('bad3.png'),img('bad4.png'),img('bad5.png'),]
 
-spaceMusic = pyglet.resource.media('space.wav')
+med = pyglet.resource.media
+
+zapSound = med('zap.wav', streaming=False)
+blastSound = med('blast.wav', streaming=False)
+
+spaceMusic = med('space.wav')
 
 scoreLabel = pyglet.text.Label(text="Score: " + str(playerScore), x=5, y=485, batch=batchDraw, group=fgroup)
 
@@ -36,78 +34,82 @@ musicPlayer.queue(spaceMusic)
 kb = pyglet.window.key
 
 
+Spr = pyglet.sprite.Sprite
+
+background = Spr(img=spaceImage, x=0, y=0, batch=batchDraw, group=bgroup)
+
 playerImage.anchor_y = playerImage.height // 2
+bulletImage.anchor_y = bulletImage.height // 2
 
-background = pyglet.sprite.Sprite(img=spaceImage, x=0, y=0, batch=batchDraw, group=bgroup)
+playerSprite = Spr(img=playerImage, x=0, y=250, batch=batchDraw, group=fgroup)
 
-playerSprite = pyglet.sprite.Sprite(img=playerImage, x=0, y=250, batch=batchDraw, group=fgroup)
 playerSprite.motion = 0
 
-def movePlayer():
-    if playerSprite.y > 0 and playerSprite.y < 500:
-        playerSprite.y += playerSprite.motion
-    elif playerSprite.y <= 0:
-        playerSprite.y = 1
-    else:
-        playerSprite.y = 499
+activeBads = []
+activeBullets = []
 
-livingBads = []
-flyingBullets = []
+def movePlayer():
+ if playerSprite.y < 0:
+  playerSprite.y = 0
+ elif playerSprite.y > 500:
+  playerSprite.y = 500
+ playerSprite.y += playerSprite.motion
+
+def addBad():
+ newBad = Spr(img=badImages[randint(0,4)], x=500, y=randint(50,450), batch = batchDraw, group = fgroup)
+ newBad.xmotion = -0.1 - random()
+ newBad.ymotion = random() * 3 - 1.5
+ activeBads.append(newBad)
+
+def moveBad(bad):
+ bad.x += bad.xmotion
+ bad.y += bad.ymotion
+ if bad.y > (500 - bad.height):
+  bad.ymotion = -1.5 * random()
+ elif bad.y < 0:
+  bad.ymotion = 1.5 * random()
+ if bad.x < 0:
+  gameOver()
+
+def gameOver():
+ global gameOn
+ scoreLabel.width=400
+ scoreLabel.multiline = True
+ scoreLabel.text = "GAME OVER You scored: " + str(playerScore)
+ scoreLabel.x = 100
+ scoreLabel.y = 300
+ scoreLabel.font_size = 40
+ gameOn = False
 
 def checkKills():
     global playerScore
-    for i in range(len(flyingBullets)):
-        for j in range(len(livingBads)):
-            if (flyingBullets[i].x > livingBads[j].x - 20
-                and flyingBullets[i].x < livingBads[j].x + livingBads[j].image.width + 20
-                and flyingBullets[i].y > livingBads[j].y
-                and flyingBullets[i].y < livingBads[j].y + livingBads[j].image.height):
-                livingBads[j].delete()
-                del livingBads[j]
-                flyingBullets[i].delete()
-                del flyingBullets[i]
+    for i in range(len(activeBullets)):
+        for j in range(len(activeBads)):
+            if (activeBullets[i].x > activeBads[j].x - 20
+                and activeBullets[i].x < activeBads[j].x + activeBads[j].image.width + 20
+                and activeBullets[i].y > activeBads[j].y
+                and activeBullets[i].y < activeBads[j].y + activeBads[j].image.height):
+                activeBads[j].delete()
+                del activeBads[j]
+                activeBullets[i].delete()
+                del activeBullets[i]
                 playerScore += 1
                 scoreLabel.text="Score: " + str(playerScore)
                 blastSound.play()
                 return
 
-def moveBad(bad):
-    global gameOn
-    bad.x -= bad.xmotion
-    bad.y += bad.motion
-    if bad.y > 450:
-        bad.motion = -1.5 * random()
-    elif bad.y < 50:
-        bad.motion = 1.5 * random()
-    if bad.x < 0:
-        scoreLabel.width=400
-        scoreLabel.multiline = True
-        scoreLabel.text = "GAME OVER You scored: " + str(playerScore)
-        scoreLabel.x = 100
-        scoreLabel.y = 300
-        scoreLabel.font_size = 40
-        gameOn = False
-
-def addBad():
-    livingBads.append(pyglet.sprite.Sprite(img=badImages[randint(0,4)], x=550, y=450-randint(0,400), batch=batchDraw, group=fgroup))
-    if len(livingBads) > 0:
-        livingBads[-1].motion = random()*3 - 1.5
-        livingBads[-1].xmotion = 0.1+random()
-
 def moveThings(dt):
-    global gameTime
-    gameTime += 1
     movePlayer()
     checkKills()
-    if len(livingBads) <= playerScore/10:
+    if len(activeBads) <= playerScore/10:
         addBad()
-    for bad in livingBads:
+    for bad in activeBads:
         moveBad(bad)
-    for i in range(len(flyingBullets)):
-        flyingBullets[i].x += flyingBullets[i].motion
-        if flyingBullets[i].x > 500:
-            flyingBullets[i].delete()
-            del flyingBullets[i]
+    for i in range(len(activeBullets)):
+        activeBullets[i].x += activeBullets[i].motion
+        if activeBullets[i].x > 500:
+            activeBullets[i].delete()
+            del activeBullets[i]
             break
 
 @window.event
@@ -118,9 +120,9 @@ def on_key_press(symbol, modifiers):
         elif symbol == kb.S:
             playerSprite.motion = -3
         elif symbol == kb.F:
-            b = pyglet.sprite.Sprite(img=bulletImage, x=playerSprite.x, y=playerSprite.y, batch=batchDraw, group=fgroup)
+            b = Spr(img=bulletImage, x=playerSprite.x, y=playerSprite.y, batch=batchDraw, group=fgroup)
             b.motion = 20
-            flyingBullets.append(b)
+            activeBullets.append(b)
             zapSound.play()
 
 @window.event
